1460c64b2f519a86fa59db83d9cfcedfa295aed3
gniloyromasno@gmail.com <romasno12345>
2022-07-15 13:45:21 +0300

    Open "https://github.com/Romasno"

--- Tests/Sample.py
+++ Tests/Sample.py
@@ -3,11 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
 import os
+import time
 
 try:
-    link = ""
+    link = "https://github.com/Romasno"
     browser = webdriver.Chrome(service=Service('../drivers/chromedriver.exe'))
     browser.get(link)
 
 finally:
+    time.sleep(5)
     browser.quit()
